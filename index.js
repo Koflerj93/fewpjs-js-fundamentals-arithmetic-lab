68a4989cad7e4ce8cd400ceee4bb23927353690c
Koflerj93 <koflerj93@gmail.com>
2020-11-24 18:16:07 -0500

    commit tree 89f1b354b1afae853f9e068d4d07215d1e3835a8

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 1;
+const num2 = 62;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const mod = 319 % 5
+
+const max = Math.max(1, 2, 3, 4, 5, 10, 20, 11)
+
